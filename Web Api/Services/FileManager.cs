d9a975dbc9dba91b07b791d5cac319da67e2a496
Alex-Za <38041377+Alex-Za@users.noreply.github.com>
2020-02-03 23:42:56 +0200

    added writing files

--- Web Api/Services/FileManager.cs
+++ Web Api/Services/FileManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,16 +10,29 @@ namespace Web_Api.Services
     {
         public void Close(int id)
         {
-            
+            allFiles[id].Close();
         }
 
         public int GetID(string extension)
         {
-            return 1;
+            this.extension = extension;
+            Random rnd = new Random();
+            return rnd.Next(100000);
         }
-
+        string extension;
+        Dictionary<int, Writer> allFiles = new Dictionary<int, Writer>();
         public void Write(int id, byte[] buffer)
         {
+
+            if (!allFiles.ContainsKey(id))
+            {
+                allFiles.Add(id, new Writer());
+                allFiles[id].Write(buffer, id, extension);
+            }
+            else
+            {
+                allFiles[id].Write(buffer, id, extension);
+            }
 
         }
     }
---
+++ Web Api/Services/Writer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Api.Services
+{
+    public class Writer
+    {
+        FileStream fileStream;
+        public void Write(byte[] buffer, int id, string extension)
+        {
+            if (fileStream == null)
+            {
+                fileStream = new FileStream(id + "." + extension, FileMode.OpenOrCreate);
+                fileStream.Write(buffer, 0, buffer.Length);
+            }
+            else
+            {
+                fileStream.Write(buffer, 0, buffer.Length);
+            }
+        }
+        public void Close()
+        {
+            fileStream.Close();
+        }
+    }
+}
